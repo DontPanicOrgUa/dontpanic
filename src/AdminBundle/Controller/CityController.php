e03fd3b8c5325b9d7cf879cda294774b812bf59b
Mykyta <mp091689@gmail.com>
2017-05-26 13:18:19 +0300

    admin list cities is implemented

--- src/AdminBundle/Controller/CityController.php
+++ src/AdminBundle/Controller/CityController.php
@@ -4,14 +4,20 @@ namespace AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class CityController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/cities", name="admin_cities_list")
+     * @Method("GET")
      */
-    public function indexAction()
+    public function listAction()
     {
-        return $this->render('AdminBundle:City:index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $cities = $em->getRepository('WebBundle:City')->findAll();
+        return $this->render('AdminBundle:City:list.html.twig', [
+            'cities' => $cities
+        ]);
     }
 }
