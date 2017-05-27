f3f64414bfb4801b8ca14181a15dbc46a4409fd3
Mykyta <mp091689@gmail.com>
2017-05-28 00:49:37 +0300

    pagination for payments is implemented

--- src/AdminBundle/Controller/PaymentController.php
+++ src/AdminBundle/Controller/PaymentController.php
@@ -3,6 +3,7 @@
 namespace AdminBundle\Controller;
 
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -12,13 +13,26 @@ class PaymentController extends Controller
     /**
      * @Route("/payments", name="admin_payments_list")
      * @Method("GET")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function listAction()
+    public function listAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
+//        $paymentsQuery = $em
+//            ->getRepository('WebBundle:Payment')
+//            ->createQueryBuilder('p')
+//            ->getQuery();
         $payments = $em->getRepository('WebBundle:Payment')->findAll();
+        $paginator  = $this->get('knp_paginator');
+        $result = $paginator->paginate(
+            $payments,
+            $request->query->getInt('page', 1),
+            $request->query->getInt('limit', 5)
+        );
+
         return $this->render('AdminBundle:Payment:list.html.twig', [
-            'payments' => $payments
+            'payments' => $result
         ]);
     }
 }
