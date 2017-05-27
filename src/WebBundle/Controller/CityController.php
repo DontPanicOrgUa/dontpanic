278d2e6bf7be35e97aab04e91b310c0063c574e0
Mykyta <mp091689@gmail.com>
2017-05-27 16:32:37 +0300

    Empty CityContoller is created

---
+++ src/WebBundle/Controller/CityController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mykyta
+ * Date: 5/27/17
+ * Time: 4:30 PM
+ */
+
+namespace WebBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class CityController extends Controller
+{
+
+    public function indexAction()
+    {
+        //TODO: implement
+    }
+}
