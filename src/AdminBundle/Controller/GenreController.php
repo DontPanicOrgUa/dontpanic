d240a7733a418422cc3d0e35b43f933d170cfc6c
Mykyta <mp091689@gmail.com>
2017-05-27 20:47:14 +0300

    Genre feature is done

---
+++ src/AdminBundle/Controller/GenreController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace AdminBundle\Controller;
+
+
+use AdminBundle\Form\GenreFormType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use WebBundle\Entity\Genre;
+
+class GenreController extends Controller
+{
+    /**
+     * @Route("/genres", name="admin_genres_list")
+     * @Method("GET")
+     */
+    public function listAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $genres = $em->getRepository('WebBundle:Genre')->findAll();
+        return $this->render('AdminBundle:Genre:list.html.twig', [
+            'genres' => $genres
+        ]);
+    }
+
+    /**
+     * @Route("/genres/add", name="admin_genres_add")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function addAction(Request $request)
+    {
+        $form = $this->createForm(GenreFormType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $genre = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($genre);
+            $em->flush();
+            $this->addFlash('success', 'New genre is added.');
+            return $this->redirectToRoute('admin_genres_list');
+        }
+
+        return $this->render('AdminBundle:Genre:add.html.twig', [
+            'genreForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/genres/{id}/edit", name="admin_genres_edit")
+     * @param Request $request
+     * @param Genre $genre
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function editAction(Request $request, Genre $genre)
+    {
+        $form = $this->createForm(GenreFormType::class, $genre);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $genre = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($genre);
+            $em->flush();
+            $this->addFlash('success', 'Genre is edited.');
+            return $this->redirectToRoute('admin_genres_list');
+        }
+
+        return $this->render('AdminBundle:Genre:edit.html.twig', [
+            'genreForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("genres/{id}/delete", name="admin_genres_delete")
+     * @param Genre $genre
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function deleteAction(Genre $genre)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($genre);
+        $em->flush();
+        $this->addFlash('success', 'Genre is deleted.');
+        return $this->redirectToRoute('admin_genres_list');
+    }
+}
---
+++ src/AdminBundle/Form/GenreFormType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace AdminBundle\Form;
+
+
+use WebBundle\Entity\Genre;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class GenreFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titleRu', TextType::class, [
+                'label' => 'Genre [RU]',
+                'attr' => [
+                    'placeholder' => 'Приключения'
+                ]
+            ])
+            ->add('titleEn', TextType::class, [
+                'label' => 'Genre [EN]',
+                'attr' => [
+                    'placeholder' => 'Adventure'
+                ]
+            ])
+            ->add('titleDe', TextType::class, [
+                'label' => 'Genre [DE]',
+                'attr' => [
+                    'placeholder' => 'Abenteuer'
+                ]
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Save Genre',
+                'attr' => [
+                    'class' => 'btn-primary',
+                    'formnovalidate' => true
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Genre::class
+        ]);
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'admin_bundle_city_form_type';
+    }
+}
