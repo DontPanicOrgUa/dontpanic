f628543409b0d85ff2f48eae951b436007f5dbaa
Mykyta <mp091689@gmail.com>
2017-06-13 12:18:29 +0300

    main page template is ready, need refactor

--- src/WebBundle/Controller/DefaultController.php
+++ src/WebBundle/Controller/DefaultController.php
@@ -9,6 +9,7 @@
 namespace WebBundle\Controller;
 
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
@@ -16,10 +17,26 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        dump('homepage');
-        die;
+        $em = $this->getDoctrine()->getManager();
+        $city = $request->query->get('city') ?: 'kiev';
+        /** @var $city \WebBundle\Entity\City */
+        $city = $em->getRepository('WebBundle:City')
+            ->findOneBy(['nameEn' => $city]);
+        return $this->render('WebBundle:City:list.html.twig', [
+            'city' => $city
+        ]);
+    }
+
+    /**
+     * @Route("/second", name="web_second")
+     */
+    public function secondAction()
+    {
+        dump(123);die;
     }
 }
