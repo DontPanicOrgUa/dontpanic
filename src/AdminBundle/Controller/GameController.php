45fb496ef9d62b655ff96979d5ae5179c55ffa61
Mykyta <mp091689@gmail.com>
2017-07-03 15:41:36 +0300

    gameController deleteAction is implemented

--- src/AdminBundle/Controller/GameController.php
+++ src/AdminBundle/Controller/GameController.php
@@ -9,7 +9,29 @@
 namespace AdminBundle\Controller;
 
 
-class GameController
-{
+use WebBundle\Entity\Game;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+class GameController extends Controller
+{
+    public function addAction()
+    {
+        // TODO: implement
+    }
+
+    /**
+     * @Route("/rooms/{slug}/games/{id}/delete", name="admin_games_delete")
+     * @param $slug
+     * @param Game $game
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function deleteAction($slug, Game $game)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($game);
+        $em->flush();
+        $this->addFlash('success', 'Game is deleted.');
+        return $this->redirectToRoute('admin_rooms_schedule', ['slug' => $slug]);
+    }
 }
